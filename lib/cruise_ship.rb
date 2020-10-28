efd32e92cdcda92ddc4770dd97678d3be76f8b2b
Dana Bliss <danajackson2@gmail.com>
2020-10-28 17:58:31 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,10 +6,21 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-def select_winner(hash)
-  hash.each{|suite, name|
-  if suite == :suite_a && name[0] == "A"
-    return name
+def select_winner(passengers)
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
   end
-  }
-end  
+end
+ 
+winner 
+end
+
+#def select_winner(hash)
+#  hash.each{|suite, name|
+#  if suite == :suite_a && name[0] == "A"
+#    return name
+#  end
+#  }
+#end  
